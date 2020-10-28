e07a0651fdac3e35a3c5c187e5604f5d3002e16b
YuriyGorodzhiy <72387638+YuriyGorodzhiy@users.noreply.github.com>
2020-10-28 20:08:07 +0300

    Update reg.php

--- eshop/system/controllers/users/reg.php
+++ eshop/system/controllers/users/reg.php
@@ -18,10 +18,18 @@ $info = mysqli_fetch_assoc($result);
 $amount = $info['num'];
 
 if ($amount > 0) {
-    echo "Такой логин или E-mail уже существуют";
+    // echo "Такой логин или E-mail уже существуют";
+    header('Location: '.$_SERVER['HTTP_REFERER'].'?wrong=1');
 } else {
     // создаём новую строчку в таблице
     mysqli_query($connect->getConnection(),"INSERT INTO core_users(login,email,password) VALUES('$login','$email','$password')");
-    echo "Вы успешно зарегистрировались";
+    // echo "Вы успешно зарегистрировались";
+
+    // получаем id только что зарегистрированного пользователя
+    $result = mysqli_query($connect->getConnection(), "SELECT * FROM core_users WHERE login='$login' OR email='$login'   ");
+    $user = mysqli_fetch_assoc($result);
+    
+    setcookie('user_id',$user['id'],time() + 3600, '/');    
+    header('Location: http://localhost/eshop/catalog.php');
 }
 
